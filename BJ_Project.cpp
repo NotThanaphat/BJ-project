f70c69bc20991b93def406345e1a8a59050c7b8c
NotThanaphat <thanapat.pingkaew@gmail.com>
2026-02-09 23:03:09 +0700

    2

--- BJ_Project.cpp
+++ BJ_Project.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include <algorithm>
 using namespace std;
 
 struct card{
@@ -25,6 +25,28 @@ void creatDeck(){
     string ranks[] = {"2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "10" , "10" , "10" , "10" , "11"};
     int values[] = {2,3,4,5,6,7,8,9,10,10,10,10,11};
     
+    deck.clear();
+    for(const &s : suits){
+        for(int i = 0; i < 13; i++){
+            card c;
+            c.suit = s;
+            c.rank = rank[i];
+            c.value= values[i];
+            deck.push_back(c);
+
+        }
+    }
+    
+}
+
+void shuffleDeck(){
+    random_shuffle(deck.begin(), deck.end());
+}
+
+card drawcard(){
+    card c = deck.back();
+    deck.pop_back ();
+    return c;
 }
 
 void useItemUndo(){
