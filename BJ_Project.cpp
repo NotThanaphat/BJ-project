c41303a165b34a0e69284051d623a94409d2eedd
NotThanaphat <thanapat.pingkaew@gmail.com>
2026-02-24 02:14:18 +0700

    8

--- BJ_Project.cpp
+++ BJ_Project.cpp
@@ -69,12 +69,12 @@ int calculateScore(const vector<card> &hand){
 void solveQuestion(){
     Question.push_back({"วัตถุมวล 5 กิโลกรัม วางอยู่บนพื้นลื่น มีแรงขนาด 20 นิวตัน มากระทำในแนวราบ วัตถุจะเคลื่อนที่ด้วยความเร่งเท่าใด", 4});
     Question.push_back({"ถ้าต้องการให้วัตถุมวล 10 กิโลกรัม เคลื่อนที่ด้วยความเร่ง 3 $m/s^2$ จะต้องใช้แรงขนาดกี่นิวตัน", 30});
-    Question.push_back({"วัตถุชิ้นหนึ่งถูกแรง 50 นิวตัน กระทำแล้วเกิดความเร่ง 5 $m/s^2$ วัตถุนี้มีมวลกี่กิโลกรัม"}, 10);
-    Question.push_back({"แรงสองแรงขนาด 10 นิวตัน และ 15 นิวตัน กระทำต่อวัตถุในทิศทางเดียวกัน แรงลัพธ์มีขนาดกี่นิวตัน"}, 25);
+    Question.push_back({"วัตถุชิ้นหนึ่งถูกแรง 50 นิวตัน กระทำแล้วเกิดความเร่ง 5 $m/s^2$ วัตถุนี้มีมวลกี่กิโลกรัม", 10});
+    Question.push_back({"แรงสองแรงขนาด 10 นิวตัน และ 15 นิวตัน กระทำต่อวัตถุในทิศทางเดียวกัน แรงลัพธ์มีขนาดกี่นิวตัน", 25});
     Question.push_back({"แรงขนาด 12 นิวตัน และ 8 นิวตัน กระทำต่อวัตถุในทิศทางตรงข้ามกัน แรงลัพธ์จะมีขนาดกี่นิวตัน", 4});
-    Question.push_back({"น้ำหนักของวัตถุมวล 2 กิโลกรัม บนผิวโลกมีค่าประมาณกี่นิวตัน (กำหนดให้ g = 9.8 m/s^2$)"}, 19.6);
+    Question.push_back({"น้ำหนักของวัตถุมวล 2 กิโลกรัม บนผิวโลกมีค่าประมาณกี่นิวตัน (กำหนดให้ g = 9.8 m/s^2$)", 19.6});
     Question.push_back({"วัตถุมวล 4 กิโลกรัม เคลื่อนที่ด้วยความเร็วคงที่ 10 เมตรต่อวินาที แรงลัพธ์ที่กระทำต่อวัตถุนี้มีค่ากี่นิวตัน", 0});
-    Question.push_back({"ออกแรงผลักกล่องขนาด 100 นิวตัน แต่กล่องยังคงอยู่นิ่ง แรงเสียดทานสถิตที่เกิดขึ้นในขณะนั้นมีขนาดกี่นิวตัน"}, 0);
+    Question.push_back({"ออกแรงผลักกล่องขนาด 100 นิวตัน แต่กล่องยังคงอยู่นิ่ง แรงเสียดทานสถิตที่เกิดขึ้นในขณะนั้นมีขนาดกี่นิวตัน", 0});
     Question.push_back({"แขวนวัตถุมวล 3 กิโลกรัม ไว้กับเพดานด้วยเชือก แรงดึงในเส้นเชือก(T)มีค่ากี่นิวตัน (กำหนดให้ g = 10 m/s^2)", 30});
     Question.push_back({"วัตถุมวล 5 kg ตกแบบเสรี (Free fall) แรงลัพธ์ที่กระทำต่อวัตถุขณะกำลังตกมีค่ากี่นิวตัน (กำหนด g = 10 m/s^2$)", 50});
 }
@@ -82,20 +82,26 @@ void solveQuestion(){
 bool doPhysicQuestion(){
     int idx = rand() % Question.size();
     cout << "\n[PHYSIC CHALLANGE] Answer correctly to use the Item!!!" << endl;
-    cout << "Question: " << Question[idx].text << endl;
+    cout << "Question: " << Question[idx].question << endl;
     cout << "Your answer: " ;
     float ans;
     cin >> ans;
 
     if(abs(ans - Question[idx].correctAnswer) < 0.1 ) {
         cout << "Correct! Item Activated.\n";
+        return true;
     }else{
         cout << "Wrong! Item Failed.\n";
+        return false;
     }
 }
 
-    
-}
+void itemPeek(){
+    if(doPhysicQuestion()){
+        cout << ">> ACTIVITED: The Dealer's hidden card is [" << DealerHand[0].rank << "-" << DealerHand[0].suit << "]\n";
+    }
+} 
+
 
 
 void useItemUndo(){
@@ -122,12 +128,17 @@ void displayHand(string name, const vector<card> &hand, bool hideFirstCard = fal
 int main(){
 
     srand(time(0));
+
+    doPhysicQuestion();
+
     char playAgain = 'y';
     while(playAgain == 'y'){
         creatDeck ();
         shuffleDeck ();
         PlayerHand.clear ();
         DealerHand.clear ();
+
+        bool itemUsed = false;
 
         PlayerHand.push_back(drawcard());
         DealerHand.push_back(drawcard());
@@ -147,7 +158,7 @@ int main(){
                 break;
             }
 
-            cout << "Action: [1] Hit [2] Stand \n";
+            cout << "Action: [1] Hit [2] Stand [3] Use Item\n";
             cout << "Select: ";
             int action;
             cin >> action;
@@ -155,10 +166,21 @@ int main(){
             if(action == 1){
                 cout << "You Hit...\n";
                 PlayerHand.push_back(drawcard());
-            }else{
+            }else if(action == 2){
                 cout << "You Stand.\n";
                 playerTurn = false;
             }
+            else if(action == 3){
+                if(itemUsed == true){
+                    cout << "[SYSTEM] MAGIC WORK\n";
+                    PlayerHand.pop_back();
+                    itemUsed = true;
+                }else{
+                    cout << "[SYSTRM] MAGIC FAIL\n";
+                    itemUsed = true;
+                }
+            }
+            
         }
         
         int PlayerScore = calculateScore(PlayerHand);
@@ -195,6 +217,7 @@ int main(){
 
         cout << "\nPlay Again? (y/n): ";
         cin >> playAgain;
-    }   
+    } 
+      
     return 0;
 }
